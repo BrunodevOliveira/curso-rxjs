916349f835bfab07b1fcdd70ae041b3cf877df3d
Bruno Oliveira <brunodevoliveira@gmail.com>
2023-11-26 00:55:06 -0300

    Seção 3 - Criando primeiro Observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
+import { Observable, Observer } from "rxjs";
+
+const observer: Observer<any> = {
+  next: v => console.log('Próximo valor [next]:', v),
+  error: e => console.error("Error [obs]", e),
+  complete: () => console.log("Completado [obs]")
+}
+
+const obs$ = new Observable<string>(subscriber => {
+  subscriber.next("Olá")
+  subscriber.next("Mundo")
+
+  //Forçar um erro:
+  // const a = undefined
+  // a.nome = "Bruno"
+
+  subscriber.complete()
+
+  subscriber.next("Subscriber morreu ao completar")
+})
 
 
-console.log('Hola Mundo!');
+// obs$.subscribe({
+//   next: (v) => console.log('next:', v),
+//   error: (e) => console.error('error:', e),
+//   complete: () => console.info("Completado")
+// })
 
-
-
-
-
-
-
+obs$.subscribe(observer)
